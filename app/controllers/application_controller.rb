9381763fa19a38f70e3726fea41e2e793e167e0c
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-05-18 08:49:35 +0100

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    attributes = %i[name photo bio]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
