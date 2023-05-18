062d193cdbe4daab21452447d742a8041f5734df
Lancelot-SO <fsowah001@gmail.com>
2023-05-18 08:03:59 +0000

    modified readme file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
   def configure_permitted_parameters
     attributes = %i[name photo bio]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
