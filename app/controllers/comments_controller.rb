ebb39db20c8adbc6a425873eded5c4f9dcb934f0
Lancelot-SO <fsowah001@gmail.com>
2023-05-19 13:45:47 +0000

    modify readme file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,16 @@ class CommentsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
   end
-  
+
   def index
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @comments = @post.comments
     respond_to do |format|
-      format.json {render json: @comments }
+      format.json { render json: @comments }
     end
   end
-  
+
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
@@ -21,13 +21,13 @@ class CommentsController < ApplicationController
     @comment.author = @user
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to request.referrer, notice: 'comment was successfully created.'}
+        format.html { redirect_to request.referrer, notice: 'comment was successfully created.' }
         format.json { render Json: @comment, status: :created }
       else
         format.html { render :new }
-        format.json {
+        format.json do
           render json: @comment, status: :created
-        }
+        end
       end
     end
   end
