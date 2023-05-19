f85dacc29f95eab6ad558e15cd6b49961208e1b7
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-05-19 14:43:33 +0100

    Add index action and update create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,33 @@ class CommentsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
   end
+  
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    respond_to do |format|
+      format.json {render json: @comments }
+    end
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = @user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to request.referrer, notice: 'comment was successfully created.'}
+        format.json { render Json: @comment, status: :created }
+      else
+        format.html { render :new }
+        format.json {
+          render json: @comment, status: :created
+        }
+      end
+    end
+  end
 
   def destroy
     @comment = Comment.find(params[:id])
